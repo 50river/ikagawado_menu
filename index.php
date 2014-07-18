311005cab8d622a505339ed515a462c316574881
50river <mr.50river@gmail.com>
2014-07-18 20:02:00 +0900

    no message

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="initial-scale=1.0,device-width,target-densitydpi=high-dpi,user-scalable=0" />
+    <link rel="stylesheet" type="text/css" href="library/normalize-2.0.1.css">
+    <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="library/main.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <title>Ikagawa-Do Drink Menu</title> 
+  </head>
+
+  <body>
+    <h1>cafe? IKAGAWA DO</h1>
+    <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          espresso
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse">
+      <div class="panel-body">
+        <span class="label label-success">エスプレッソ</span>
+      <span class="label label-warning">スチームドミルク</span>
+      <span class="label label-danger">フォームドミルク</span>
+      <span class="label label-info">アイスミルク</span>
+
+      <li>ソロ 310</li>
+      <div class="progress">
+        <div class="progress-bar progress-bar-success" style="width: 10%">
+          <span class="sr-only">40cc エスプレッソ</span>
+        </div>
+      </div>
+      <li>ドッピオ 520</li>
+      <div class="progress">
+        <div class="progress-bar progress-bar-success" style="width: 20%">
+          <span class="sr-only">80cc エスプレッソ</span>
+        </div>
+      </div>
+      <li>カフェラテ 420</li>
+      <div class="progress">
+        <div class="progress-bar progress-bar-success" style="width: 10%">
+          <span class="sr-only">40cc エスプレッソ</span>
+        </div>
+        <div class="progress-bar progress-bar-warning" style="width: 40%">
+          <span class="sr-only">160cc スチームドミルク</span>
+        </div>
+      </div>
+      <li>アイスカフェラテ 470</li>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          softdrink
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+      <li>ミルク（ホットorアイス） 310</li>
+      <li>オレンジジュース 310</li>
+      <li>グレープジュース 310</li>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          tea
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        <li>English breakfast 520</li>
+      <li>Darjeeling earlgray 520</li>
+      <li>Yerba mate 520</li>
+      <li><span class="label label-primary">caffeine free</span>Lemon & Ginger 520</li>
+      <li><span class="label label-primary">caffeine free</span>Super fruit 520</li>
+      <li><span class="label label-primary">caffeine free</span>Chai tea 520</li>
+      <li><span class="label label-primary">caffeine free</span>Chamomile flowers 520</li>
+      <li><span class="label label-primary">caffeine free</span>Rooibos 520</li>
+      <li><span class="label label-primary">caffeine free</span>Spiced winter red tea 520</li>
+      <li><span class="label label-primary">caffeine free</span>Peppermint leaves 520</li>
+      </div>
+    </div>
+  </div>
+</div>
+  </body>
+</html>
